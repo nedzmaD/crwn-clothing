3c28a78ac7554abc6bd65839b0d677b192837482
star1509 <39267238+star1509@users.noreply.github.com>
2021-03-10 00:21:17 +0300

    adding header to the pages together with logo and links to the shop and contact page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -14,7 +16,7 @@ import './pages/homepage/homepage.syles.scss';
 function App() {
   return (
     <div> 
-    
+   <Header/>
    <Switch>
      <Route exact path='/' component={HomePage}/>
      <Route  path='/shop' component={ShopPage} />
