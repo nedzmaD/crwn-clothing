991174d7bab87f040d8e27e16c99fa6e0cc308e0
star1509 <39267238+star1509@users.noreply.github.com>
2021-03-12 16:12:33 +0300

    implemented firebase utils, icluded ability to authenticate users

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 import './pages/homepage/homepage.syles.scss';
@@ -29,10 +29,23 @@ class App extends React.Component{
 unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=> {
+      if(userAuth){
 
-      console.log(user);
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser:{
+              id: snapShot.id, 
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+      //If user logs out set state to null
+      this.setState({currentUser: userAuth});
+
     });
   }
   
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+//Inside config copied from firebase while creating a project
 const config = {
     apiKey: "AIzaSyCOwXSRtSdyyXLsi4q4dj3A48AEICzs3To",
     authDomain: "crwn-db-32d24.firebaseapp.com",
@@ -11,6 +12,38 @@ const config = {
     appId: "1:1003381240266:web:05a194f22791d19322df33",
     measurementId: "G-L3J5SELF1B"
   };
+
+//Store user's data into db
+export const createUserProfileDocument = async (userAuth, additionalData)=> {
+  //If user is not signed do nothing
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  //If snapshot doesn't exist create a user
+  if (!snapShot.exists){
+    //Display name and email of user obj
+    const {displayName, email} = userAuth;
+    //Current date of current time this is invoked
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    }
+    catch(error) {
+      console.log('error creating user', error.message);
+
+    }
+  }
+  return userRef;
+};
 
   firebase.initializeApp(config);
 
@@ -22,3 +55,4 @@ const config = {
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
   export default firebase;
+  
